26c2ea60aa1d5a425243c97fff37f657b0d8cff8
Dremalka <dremalka@ua.fm>
2015-09-07 13:40:27 +0600

    Удалена функция getlistbots и тест к ней. Вместо этого будет вызываться метод GetListBots менеджера ботов.

--- api.go
+++ api.go
@@ -16,21 +16,11 @@ type infbot struct {
 func listbot(c *echo.Context) error {
 	//	TODO сформировать список ботов и отправить клиенту
 	// временные данные
-	list, err := getlistbot()
+	list, err := MB.GetListBots()
 	if err != nil {
 		return c.JSON(http.StatusConflict, nil)
 	}
 	return c.JSON(http.StatusOK, list)
-}
-
-// готовит массив с информацией о ботах
-func getlistbot() ([]infbot, error) {
-	list := make([]infbot, 2)
-	list[0].ID = "123"
-	list[0].Name = "abc"
-	list[1].ID = "456"
-	list[1].Name = "dfg"
-	return list, nil
 }
 
 func createbot(c *echo.Context) error {
