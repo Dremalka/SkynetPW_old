86ade20414045c9236c9fe208a4663175a46f093
Dremalka <dremalka@ua.fm>
2015-09-07 13:43:24 +0600

    Добавлена структура с данными ботов для вывода в веб-интерфейсе. Добавлен метод менеджера ботов GetListBots. Формирует массив текущих ботов.

--- managerbots.go
+++ managerbots.go
@@ -10,3 +10,30 @@ func newManagerBots() (*ManagerBots, error) {
 	mb.ListBot = make(map[string]*Bot)
 	return mb, nil
 }
+
+//Inf структура с информацией по боту для веб-интерфейса
+type Inf struct {
+	ID 			string
+	Name 		string
+	Server 		string
+	Login 		string
+	Password 	string
+}
+
+//GetListBots метод формирует массив с текущими ботами
+func (mb *ManagerBots) GetListBots() ([]Inf, error) {
+	var botinf Inf
+	var list []Inf
+	var id string
+	var bot *Bot
+
+	for id, bot = range mb.ListBot {
+		botinf.ID = id
+		botinf.Name = bot.Name
+		botinf.Server = bot.Server
+		botinf.Login = bot.Login
+		botinf.Password = bot.Password
+		list = append(list, botinf)
+	}
+	return list, nil
+}
