d8236c99bd22de3e45413a6d4c4b5000c6977b62
Dremalka <dremalka@ua.fm>
2015-09-07 12:47:21 +0600

    Функция listbot вызывает функцию getlistbot и список возвращает в формате json. Функция getlistbot добавлена для упрощения тестирования. Временно создает фиктивный массив. В дальнейшем будет форматировать массив ботов.

--- api.go
+++ api.go
@@ -6,27 +6,38 @@ import (
 )
 
 // api
+
+type infbot struct {
+	ID   string
+	Name string
+}
+
 // отправляет json-ответ с массивом текущих ботов
 func listbot(c *echo.Context) error {
 	//	TODO сформировать список ботов и отправить клиенту
 	// временные данные
-	type tmpstr struct {
-		ID string
-		Name string
+	list, err := getlistbot()
+	if err != nil {
+		return c.JSON(http.StatusConflict, nil)
 	}
-	list := make([]tmpstr, 2)
+	return c.JSON(http.StatusOK, list)
+}
+
+// готовит массив с информацией о ботах
+func getlistbot() ([]infbot, error) {
+	list := make([]infbot, 2)
 	list[0].ID = "123"
 	list[0].Name = "abc"
 	list[1].ID = "456"
 	list[1].Name = "dfg"
-
-	return c.JSON(http.StatusOK, list)
+	return list, nil
 }
+
 func createbot(c *echo.Context) error {
 	//	TODO создать нового бота
 	return c.String(http.StatusOK, "ok\n")
 }
-func sendactiontobot (c *echo.Context) error {
+func sendactiontobot(c *echo.Context) error {
 	//	TODO переслать команду боту (старт, стоп...)
 	return c.String(http.StatusOK, "ok\n")
 }
@@ -35,13 +46,11 @@ func deletebot(c *echo.Context) error {
 	return c.String(http.StatusOK, "ok\n")
 }
 
-
 //websocket
 func websockdatabots(c *echo.Context) error {
 	//	TODO создать вебсокет для динамического обмена данными
 	return nil
 }
-
 
 //отладочные функции
 func updateinfbots(c *echo.Context) error {
