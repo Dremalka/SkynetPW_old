801299e5f8e7f1e0290604a5adef3a372d2ef6ec
Dremalka <dremalka@ua.fm>
2015-09-09 19:49:39 +0600

    Добавлена функция создания нового объекта бота.

--- bot.go
+++ bot.go
@@ -9,3 +9,14 @@ type Bot struct {
 	Password	string
 	status 		string
 }
+
+// newBot создать новый объект бота
+func newBot(infbot map[string]string) (*Bot, error) {
+	bot := &Bot{}
+	bot.Name = infbot["name"]
+	bot.Server = infbot["server"]
+	bot.Login = infbot["login"]
+	bot.Password = infbot["password"]
+
+	return bot, nil
+}
